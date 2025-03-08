e6f012c932e006bc02bfea8544f221dea9b94f4e
akshathkashyap <akshath.kp99@gmail.com>
2025-03-09 00:26:06 +0530

    Fix home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,19 @@ import Collage from "@/components/Collage";
 
 export default function Home() {
 	return (
-		<div className="flex justify-center items-center w-full h-full">
+		<div className='flex justify-center items-center w-full h-full'>
 			<section>
-				<span className="flex flex-row gap-18 justify-center">
+				<span className='flex flex-row gap-3 lg:gap-18 justify-center'>
 					<Image
-						className='w-[8rem] object-contain animate-[spin_1s_ease-in-out]'
+						className='w-12 lg:w-32 object-contain animate-[spin_1s_ease-in-out]'
 						src={"/salesway_logo.png"}
 						alt='salesway_logo.png'
 						width={560}
 						height={560}
 					></Image>
-					<h1 className='font-bold text-9xl text-black'>Salesway</h1>
+					<h1 className='font-bold text-5xl lg:text-9xl text-black'>
+						Salesway
+					</h1>
 				</span>
 				<Collage></Collage>
 			</section>
--- src/components/Collage.tsx
+++ src/components/Collage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function CollageCard({ children }: { children: React.ReactNode }) {
 	return (
-		<div className='w-[24rem] h-[20rem] bg-white rounded-4xl shadow-2xl outline-2 overflow-hidden'>
+		<div className='w-[24rem] h-[20rem] p-4 bg-white shadow-2xl translate-y-[100vh] -rotate-3 overflow-hidden'>
 			{children}
 		</div>
 	);
@@ -12,37 +12,33 @@ function CollageCard({ children }: { children: React.ReactNode }) {
 export default function Collage() {
 	return (
 		<section className='flex flex-row items-start gap-8 m-8'>
-			<span className=''>
-				<CollageCard>
-					<Image
-						className='object-contain'
-						src={
-							"https://airfixture.com/hubfs/pexels-pixabay-269077.jpg"
-						}
-						alt='office'
-						width={500}
-						height={500}
-					></Image>
-					<span className='block p-5 bold font-handwritten'>
-						#office
-					</span>
-				</CollageCard>
+			<span className='w-1/2 h-1/2 p-4 invisible bg-white shadow-md translate-x-8 -rotate-3 overflow-hidden animate-[polaroidAppear_0.2s_0.8s_1_forwards_ease-out]'>
+				<Image
+					className='object-contain'
+					src={
+						"https://airfixture.com/hubfs/pexels-pixabay-269077.jpg"
+					}
+					alt='office'
+					width={500}
+					height={500}
+				></Image>
+				<span className='font-bold font-handwritten block mt-2'>
+					#office
+				</span>
 			</span>
-			<span>
-				<CollageCard>
-					<Image
-						className='object-contain'
-						src={
-							"https://www.cio.com/wp-content/uploads/2025/03/3485346-0-60813400-1741173693-shutterstock_2431976515.jpg"
-						}
-						alt='teams'
-						width={500}
-						height={500}
-					></Image>
-					<span className='block p-5 bold font-handwritten'>
-						#ourTeam
-					</span>
-				</CollageCard>
+			<span className='w-1/2 h-1/2 p-4 invisible bg-white shadow-md rotate-5 overflow-hidden animate-[polaroidAppear_0.2s_1s_1_forwards_ease-out]'>
+				<Image
+					className='object-contain'
+					src={
+						"https://www.cio.com/wp-content/uploads/2025/03/3485346-0-60813400-1741173693-shutterstock_2431976515.jpg"
+					}
+					alt='teams'
+					width={500}
+					height={500}
+				></Image>
+				<span className='font-bold font-handwritten block mt-2'>
+					#ourTeam
+				</span>
 			</span>
 		</section>
 	);
